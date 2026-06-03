f54795a730b860a041158f156b5213055e742e76
david-aurel-lenoir <d.butarebit@alustudent.com>
2026-06-03 12:13:54 -0700

    comments describing proccess_vitals

--- hospital_analysis.sh
+++ hospital_analysis.sh
@@ -1,5 +1,18 @@
 #!/bin/bash
 
+# Script: hospital_analysis.sh
+# Member 5: Clinical Analyst
+# Function: process_vitals()
+# Purpose: This function scans through
+#          the heart rate and temperature
+#          log files inside active_logs/
+#          It searches for any row marked
+#          as CRITICAL using grep.
+#          It then extracts the Timestamp,
+#          Device_ID and Value using awk
+#          and saves them into
+#          reports/critical_alerts.txt
+
 process_vitals() {
     echo "Processing critical vitals"
 
