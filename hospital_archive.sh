1a203b6b572c86d19ffcb85fd44b95eafac68d86
rruzigana-hub <r.ruzigana@alustudent.com>
2026-06-03 14:08:43 +0200

    Add archive_logs function structure

---
+++ hospital_archive.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+# =============================================================
+# hospital_archive.sh
+# Member 4 - The Archivist
+# =============================================================
+
+archive_logs() {
+
+    TIMESTAMP=$(date +"%Y%m%d_%H%M")
+
+    echo "---------------------------------------------"
+    echo " KNH Log Archiving Started"
+    echo " Timestamp: $TIMESTAMP"
+    echo "---------------------------------------------"
+
+    LOG_FILES=("heart_rate.log" "temperature.log" "water_usage.log")
+
+    for FILE in "${LOG_FILES[@]}"; do
+
+        SOURCE="active_logs/$FILE"
+        FILENAME_NO_EXT="${FILE%.log}"
+        DESTINATION="archived_logs/${FILENAME_NO_EXT}_${TIMESTAMP}.log"
+
+        if [ -f "$SOURCE" ]; then
+            mv "$SOURCE" "$DESTINATION"
+            echo "[ARCHIVED]  $FILE  →  $DESTINATION"
+        else
+            echo "[SKIPPED]   $SOURCE not found."
+        fi
+
+        touch "$SOURCE"
+        echo "[RECREATED] Empty $SOURCE ready for new data."
+
+    done
+
+    echo "---------------------------------------------"
+    echo " Archiving Complete. System is live."
+    echo "---------------------------------------------"
+}
+
+archive_logs
